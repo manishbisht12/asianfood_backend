4d81a818d2f8264fdb78c9c7de3698cc6628d66f
manishbisht12 <154014370+manishbisht12@users.noreply.github.com>
2026-02-16 15:40:27 +0530

    fix cors

--- controllers/authController.js
+++ controllers/authController.js
@@ -32,7 +32,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
 
@@ -55,7 +55,9 @@ export const logout = async (req, res) => {
     res.clearCookie("token", {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "strict",
+      sameSite: "None",
+      secure: true,
+
     });
 
     return res.status(200).json({
